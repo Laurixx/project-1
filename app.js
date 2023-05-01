c466564b45b2fb6155b60aa648ce0636ce450ac0
Laurentiu Robu <43887989+Laurixx@users.noreply.github.com>
2023-05-01 07:05:58 +0100

    updating style , and media query , fixing the filt

--- app.js
+++ app.js
@@ -2,37 +2,41 @@ import {products} from "./products.js"
 import {categories} from "./categories.js"
 const productsSection = document.getElementById('products')
 const categoriesSection = document.getElementById('categories')
+const logoDiv = document.querySelector(".logo")
+
+// THIS RENDERS THE CATEGORIES 
 function renderCategories (cath){
     for(const category of categories){
         categoriesSection.innerHTML += `
         <div class="category" id="${category.name}">
-            <img src="${category.icon}" alt="">
-            <p>${category.name}</p>
+        <img src="${category.icon}" alt="">
+        <p>${category.name}</p>
         </div>
-    `
+        `
     }
-renderProducts(products)
+    renderProducts(products) // CALLS RENDER PRUCTS 
 }
+// RENDERS ALL THE PRODUCTS ON THE PAGE 
 function renderProducts(prod){
     for (const product of prod){
-productsSection.innerHTML += `
-    <div class="product" id="${product.title}">
-    <img src="pictures/${product.thumbnail}" alt="" class = "product-thumbnail">
-    <div class="product-footer">
+        productsSection.innerHTML += `
+        <div class="product" id="${product.title}">
+        <img src="pictures/${product.thumbnail}" alt="" class = "product-thumbnail">
+        <div class="product-footer">
         <div class="name-location">
-            <h3>${product.title}</h3>
-            <p>${product.location}</p>
+        <h3>${product.title}</h3>
+        <p>${product.location}</p>
         </div>
         <div class="delivery-price">
-            <p class = "deliveryNote"></p>
-            <p class ="price">${product.price} £</p>
+        <p class = "deliveryNote"></p>
+        <p class ="price">${product.price} £</p>
         </div>
-    </div>
-    </div>`
+        </div>
+        </div>`
     }
-    renderDelivery(products)
+    renderDelivery(products) // CALLS RENDER DELIVERY 
 }
-
+// THIS RENDERS THE OPTION DELIVERY OR COLLECTION TO EACH ITEM 
 function renderDelivery (prod) {
     const deliveryNote = document.getElementsByClassName('deliveryNote')
     for (let i = 0; i < prod.length; i++) {   //this iterate over the HTML Collection from the deliveryNote
@@ -44,11 +48,13 @@ function renderDelivery (prod) {
        }
 }
 }
-renderCategories(categories)
 
+renderCategories(categories)
+// THIS FILTERES PRODUCTS BY THEYR CATEGORY 
 let filteredProducts = []
 
 categoriesSection.addEventListener('click', function(e){
+    filteredProducts.splice(0, filteredProducts.length) // THIS CLEARS THE ARRAY FOR EVERY CATEGORY 
     let category = (e.target.parentElement.id);
     console.log(category);
 for (let prod of products){
@@ -58,7 +64,7 @@ for (let prod of products){
 }
 productsSection.innerHTML =``
 for (let product of filteredProducts){
-    productsSection.innerHTML=productsSection.innerHTML += `
+    productsSection.innerHTML += `
     <div class="product" id="${product.title}">
     <img src="pictures/${product.thumbnail}" alt="" class = "product-thumbnail">
     <div class="product-footer">
@@ -73,7 +79,7 @@ for (let product of filteredProducts){
     </div>
     </div>`
     }
-    renderDelivery(products)
+    renderDelivery(filteredProducts) // RENDER DELIVERY AGAIN '_' ? 
 console.log(filteredProducts);
 
 })
